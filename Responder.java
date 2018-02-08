7a14b3f455f5178039fff502122c3231ccb48d15
Aitor Diez Fernandez <aitorchic@gmail.com>
2018-02-08 12:00:23 +0100

    Modificado la clase Responder

--- Responder.java
+++ Responder.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+import java.util.ArrayList;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +9,21 @@
  */
 public class Responder
 {
+    private Random aleatorio;
+    private ArrayList<String> respuestas;
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        respuestas=new ArrayList<String>();
+        aleatorio= new Random();
+        
+        respuestas.add("Podrias darme mas detalles del problema que tienes?");
+        respuestas.add("De que marca es tu ordenador?");
+        respuestas.add("Eso no es un error es una característica");
+        respuestas.add("Que le ocurre al ordenador?");
+        respuestas.add("Cual es tu sistema operativo?");
     }
 
     /**
@@ -20,6 +32,7 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        int numeroAleatorio=aleatorio.nextInt(5);
+        return respuestas.get(numeroAleatorio);
     }
 }
